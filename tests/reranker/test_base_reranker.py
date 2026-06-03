236807c4ca6c4292e2f7802f13583f43f64029a6
nanjiu7012 <2673848854@qq.com>
2026-06-03 11:02:15 +0800

    Update test_base_reranker.py

--- tests/reranker/test_base_reranker.py
+++ tests/reranker/test_base_reranker.py
@@ -34,25 +34,25 @@ def test_rerank_scores_and_sorts():
     assert ranked[0].score > ranked[1].score
 
 
-def test_rerank_time_decay_weighting():
+def test_rerank_uniform_weighting():
     corpus = make_sample_corpus(3)
     papers = [make_sample_paper(title="P")]
 
-    # Only similar to the oldest paper (index 2 after reverse-sort by date)
+    # Similar only to one corpus paper (any position)
     sim = np.array([[0.0, 0.0, 1.0]])
     reranker = StubReranker(sim)
-    ranked_old = reranker.rerank(papers, corpus)
-    score_old = ranked_old[0].score
+    ranked_a = reranker.rerank(papers, corpus)
+    score_a = ranked_a[0].score
 
-    # Only similar to the newest paper (index 0 after reverse-sort by date)
+    # Similar only to another corpus paper
     papers2 = [make_sample_paper(title="P")]
     sim2 = np.array([[1.0, 0.0, 0.0]])
     reranker2 = StubReranker(sim2)
-    ranked_new = reranker2.rerank(papers2, corpus)
-    score_new = ranked_new[0].score
+    ranked_b = reranker2.rerank(papers2, corpus)
+    score_b = ranked_b[0].score
 
-    # Newest corpus paper gets higher time-decay weight, so score should be higher
-    assert score_new > score_old
+    # Without time decay, all corpus papers have equal weight, scores should be equal
+    assert score_a == score_b
 
 
 def test_rerank_single_candidate_single_corpus():
